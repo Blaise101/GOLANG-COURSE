db0bf840d6a7a631240d1a1093a581abea733854
Blaise202 <izerimanablaise5@gmail.com>
2026-04-21 21:58:44 +0200

    Variables & Datatypes

--- Booking-app/main.go
+++ Booking-app/main.go
@@ -13,5 +13,13 @@ func main(){
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets to attend.")
 	fmt.Print("================================\n")
+
+	var username string
+	var usertickets int
+
+	username = "kamali"
+	usertickets = 4
+
+	fmt.Printf("User %v booked %v tickets.\n", username, usertickets)
 	
 }
